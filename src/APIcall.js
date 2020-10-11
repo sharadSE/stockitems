0b71b9a04152b2ff65c802cf89ca3136c2afd8e1
sharadSE <64522516+sharadSE@users.noreply.github.com>
2020-10-11 09:51:15 +0530

    Update APIcall.js

--- src/APIcall.js
+++ src/APIcall.js
@@ -18,36 +18,6 @@ export default class APIcall extends Component {
   render() {
     return (
       <>
-        {/* <div class="wrap">
-          <div class="search">
-            <input type="text" class="searchTerm" placeholder="What are you looking for?"/>
-              <button type="submit" class="searchButton">
-                <i class="fa fa-search"></i>
-              </button>
-              
-          </div>
-          <p>
-          <input type="checkbox" />
-          {' '}Only show products in stock
-        </p>
-        </div>
-        
-          <table>
-            <tr>
-              <th>Category</th>
-              <th>Name</th>
-              <th>Stocked</th>
-              <th>Price</th>
-            </tr>
-            {this.state.repos.map(repo =>
-              <tr>
-                <td>{repo.category}</td>
-                <td>{repo.name}</td>
-                <td>{repo.stocked ? "True" : "false"}</td>
-                <td>{repo.price}</td>
-              </tr>
-            )}
-          </table> */}
           <FilterableProductTable products={this.state.repos} />,
         </>
     );
